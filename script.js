73f437fc187b1a26f740717e7c45ae2a831d293b
chih-hsi-chen <chih.hsi.chen@gmail.com>
2020-03-04 19:30:13 +0800

    detect if device supports touch event, then disable btns

--- script.js
+++ script.js
@@ -3,41 +3,25 @@ const btnL = document.querySelector('.btn-left');
 const stTrack = document.querySelector('.schedule-table');
 const tracksW = stTrack.offsetWidth;
 
-btnR.addEventListener('touchstart', (event) => {
-  stTrack.scrollBy({
-    left: 300,
-    behavior: 'smooth'
-  });
-  console.log(event); 
-});
 
-btnL.addEventListener('touchstart', (event) => {
-  stTrack.scrollBy({
-    left: -300,
-    behavior: 'smooth'
-  });
-  console.log(event);
-});
+if(!('ontouchstart' in window)) {
+	btnR.addEventListener('click', (event) => {
+		stTrack.scrollBy({
+			left: 300,
+			behavior: 'smooth'
+		});
+	});
 
-btnL.addEventListener('touchend', (event) => {
-	event.preventDefault();
-});
-btnR.addEventListener('touchend', (event) => {
-	event.preventDefault();
-});
+	btnL.addEventListener('click', (event) => {
+		stTrack.scrollBy({
+			left: -300,
+			behavior: 'smooth'
+		});
+	});
+}
+else {
+	btnR.style.display = 'none';
+	btnL.style.display = 'none';
+}
 
-btnR.addEventListener('click', (event) => {
-	console.log(event);
-  stTrack.scrollBy({
-    left: 300,
-    behavior: 'smooth'
-  });
-});
 
-btnL.addEventListener('click', (event) => {
-	console.log(event);
-  stTrack.scrollBy({
-    left: -300,
-    behavior: 'smooth'
-  });
-});
